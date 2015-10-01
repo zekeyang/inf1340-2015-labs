1749f22b0eceee3b3dbe710c0181c4f327006397
Susan Elliott Sim <ses@drsusansim.org>
2015-09-30 23:29:14 -0400

    Starter files for Lab 1

--- lab1.py
+++ lab1.py
@@ -8,3 +8,13 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
+def vowel_or_consonant():
+    """
+    Exercise: Vowel or Consonant
+    Reads a letter of the alphabet from the user. (You can assume that it's
+    lowercase.) If the user enters a, e, i, o or u then your program should
+    display "vowel". If the user enters y then your program should display
+    "sometimes a vowel, sometimes a consonant". Otherwise your program should
+    display a message indicating that the letter is a "consonant".
+    """
+
---
+++ test_lab1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+""" Test Suite for Graded Lab #1 for Inf1340, Fall 2015 """
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+import mock
+from lab1_solution import vowel_or_consonant
+
+
+def test_vowel_or_consonant(capsys):
+
+    vowels = list("aeiou")
+    for letter in vowels:
+        with mock.patch("__builtin__.raw_input", return_value=letter):
+            vowel_or_consonant()
+            (out, err) = capsys.readouterr()
+            assert out == "vowel\n"
+
+    with mock.patch("__builtin__.raw_input", return_value="y"):
+        vowel_or_consonant()
+        (out, err) = capsys.readouterr()
+        assert out == "sometimes a vowel, sometimes a consonant\n"
+
+    consonants = list("bcdfghjklmnpqrstvwxz")
+    for letter in consonants:
+        with mock.patch("__builtin__.raw_input", return_value=letter):
+            vowel_or_consonant()
+            (out, err) = capsys.readouterr()
+            assert out == "consonant\n"
