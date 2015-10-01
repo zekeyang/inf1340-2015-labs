c4aadc65a99f4b228d28a8c52134ed41d333fa00
Susan Elliott Sim <ses@drsusansim.org>
2015-09-30 22:37:11 -0400

    Added a .gitignore file

---
+++ lab1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" Graded Lab #1 for Inf1340, Fall 2015 """
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+
